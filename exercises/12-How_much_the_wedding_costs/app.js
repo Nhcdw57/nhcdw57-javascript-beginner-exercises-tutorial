5aadc28a652234d1203965566c24c325deddc106
Nhcdw57 <chocolatedounut@gmail.com>
2025-05-22 15:27:38 +0000

    Pending changes exported from your codespace

--- exercises/12-How_much_the_wedding_costs/app.js
+++ exercises/12-How_much_the_wedding_costs/app.js
@@ -3,8 +3,16 @@ let guests = prompt('How many people are coming to your wedding?');
 function getPrice(guests){
     let cost = 0;
     // Your code here
- 
-
+    guests = parseInt(guests);
+    if(50 >= guests){
+        cost = 4000;
+    }else if(100>=guests){
+        cost = 10000;
+    }else if(200>=guests){
+        cost = 15000;
+    }else if(guests > 200){
+        cost = 20000;
+    }
     return cost;
 }
 
